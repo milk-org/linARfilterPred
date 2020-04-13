52f3dc5b8d419eebfa96da57ed622077a1bf5260
oguyon <oliv.guyon@gmail.com>
2020-04-12 14:30:44 -1000

    remove 00CORE module

--- linARfilterPred.c
+++ linARfilterPred.c
@@ -81,7 +81,6 @@ int clock_gettime(int clk_id, struct timespec *t)
 
 
 #include "CommandLineInterface/CLIcore.h"
-#include "00CORE/00CORE.h"
 #include "COREMOD_tools/COREMOD_tools.h"
 #include "COREMOD_memory/COREMOD_memory.h"
 #include "COREMOD_arith/COREMOD_arith.h"
